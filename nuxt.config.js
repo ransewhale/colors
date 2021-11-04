3ba6df765d146f1217ebe7eb6cf686a752664c66
ransewhale <5298807+ransewhale@users.noreply.github.com>
2021-11-04 23:02:44 +0900

    Correct App Name

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'We have 16,777,216 colors. Which color do you like?' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
