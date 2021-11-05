d1e0b0a4698770d6b49e4d9f921b46d8a540b572
ransewhale <5298807+ransewhale@users.noreply.github.com>
2021-11-05 09:05:39 +0900

    Adjust Bars

--- components/SelectBars.vue
+++ components/SelectBars.vue
@@ -3,10 +3,10 @@
     <v-row
       v-for="(item, index) in sliderItems"
       :key="index" class="mb-0 mt-0">
-      <v-col cols="2" md="1" class="mr-0 pr-0">
-        <v-card-text :style="'color: '+item.color+';'" class="text-center text-h6 ma-0 pl-0 pr-0 pt-2">{{item.label.substring(0,1)}}</v-card-text>
+      <v-col cols="2" md="1" class="mb-0 mt-0 mr-0 pr-0 pb-0 pt-0">
+        <v-card-text :style="'color: '+item.color+';'" class="text-center text-h6 ma-0 pl-0 pr-0 pt-2 pb-0">{{item.label.substring(0,1)}}</v-card-text>
       </v-col>
-      <v-col cols="10" md="11" class="ml-0 pl-0">
+      <v-col cols="10" md="11" class="mb-0 mt-0 ml-0 pl-0 pb-0 pt-0">
         <v-slider
           v-model="item.value"
           :color="item.color"
@@ -14,6 +14,7 @@
           :thumb-color="item.color"
           :thumb-label="true"
           min="0" max="255"
+          :hide-details="true" :dense="true"
           class="mt-2 mb-2 ml-0 mr-2">
         </v-slider>
       </v-col>
